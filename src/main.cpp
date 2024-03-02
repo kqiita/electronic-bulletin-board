2fb03e803ebd37e0da8afc257489bc9c5ded4978
kqiita <134604805+kqiita@users.noreply.github.com>
2024-03-02 18:53:01 +0900

    little change former version

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,9 @@ size_t text_destinationpos = 0;
 
 void setup() {
   display.init();
-  // 画面が横長になるように回転
+  LittleFS.begin();
+  
+    // 画面が横長になるように回転
   if (display.width() < display.height()) display.setRotation(display.getRotation() ^ 1);
 
   sprite_traintype.createSprite(48,display.height()); // 画面幅+１文字分の横幅を用意
@@ -40,9 +42,6 @@ void setup() {
   sprite_destination.setTextSize(0.80);
   sprite_destination.setTextWrap(false);        // 右端到達時のカーソル折り返しを禁止
 
-  LittleFS.begin();
-  
-  
 }
 
 void loop(){
@@ -55,25 +54,5 @@ void loop(){
   sprite_destination.pushSprite(&display,48,display.height()/2 - 24/2);
   
   delay(2000);
-
-  /*
-  int32_t cursor_x = sprite.getCursorX() - 1;   // 現在のカーソル位置を取得し、1ドット左に移動
-  if (cursor_x <= 0) // カーソル位置が左端に到達している場合は一周したと判断
-  {
-    textpos = 0;            // 文字列の読取り位置をリセット
-    cursor_x = display.width(); // 新たな文字が画面右端に描画されるようにカーソル位置を変更
-  }
-
-  sprite.setCursor(cursor_x, 0); // カーソル位置を更新
-  sprite.scroll(-1, 0);          // キャンバスの内容を1ドット左にスクロール
-  while (textpos < textlen && cursor_x <= display.width()) // 画面右端に文字が書けるか判定
-  {
-    sprite.print(text[textpos++]);   // 1バイトずつ出力 (マルチバイト文字でもこの処理で動作します)
-    cursor_x = sprite.getCursorX();  // 出力後のカーソル位置を取得
-  }
-
-  sprite.pushSprite(&display, 0, 0);
-
-  delay(200);
-  */
+  
 }
